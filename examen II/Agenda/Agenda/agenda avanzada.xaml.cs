ff040a2736fbc00e75971435df06382e185ca215
Juan Carlos Manuel Lechuga Ortiz21 <134109649+jU1fr@users.noreply.github.com>
2024-04-27 11:03:09 -0600

    Add files via upload

---
+++ examen II/Agenda/Agenda/agenda avanzada.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Agenda
+{
+    /// <summary>
+    /// Lógica de interacción para agenda_2.xaml
+    /// </summary>
+    public partial class agenda_2 : Window
+    {
+        public agenda_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
